037d8ba7cd73fb5252d09e24cd4f4fb90ccec579
alinavo <alinahuynhvo@gmail.com>
2019-02-21 04:14:29 -0500

    empty friend array

---
+++ app/data/friends.js
@@ -0,0 +1,104 @@
+// friend array
+
+var friends = [
+   {
+      "name":"Harry Potter",
+      "photo":"",
+      "scores":[
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         ""
+      ]
+   },
+   {
+      "name":"Hermione Granger",
+      "photo":"",
+      "scores":[
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         ""
+      ]
+   },
+   {
+      "name":"Ronald Weasley",
+      "photo":"",
+      "scores":[
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         ""
+      ]
+   },
+   {
+      "name":"Neville Longbottom",
+      "photo":"",
+      "scores":[
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         "",
+         ""
+      ]
+   },
+	{
+	   "name":"Luna Lovegood",
+	   "photo":"",
+	   "scores":[
+	      "",
+	      "",
+	      "",
+	      "",
+	      "",
+	      "",
+	      "",
+	      "",
+	      "",
+	      ""
+	   ]
+	},
+	{
+	   "name":"Ginny Weasley",
+	   "photo":"",
+	   "scores":[
+	      "",
+	      "",
+	      "",
+	      "",
+	      "",
+	      "",
+	      "",
+	      "",
+	      "",
+	      ""
+	   ]
+	}
+
+]
+
+// Exported array
+module.exports = friends;
