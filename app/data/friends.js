efcf718ff2ad5c603e22f140038742ed92bcd101
alinavo <alinahuynhvo@gmail.com>
2019-02-21 20:51:44 -0500

    updated friend array with data

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,98 +3,99 @@
 var friends = [
    {
       "name":"Harry Potter",
-      "photo":"",
+      "photo":"https://timedotcom.files.wordpress.com/2014/07/301386_full1.jpg",
       "scores":[
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         ""
+         "3",
+         "5",
+         "4",
+         "2",
+         "4",
+         "2",
+         "5",
+         "1",
+         "4",
+         "5"
+         
       ]
    },
    {
       "name":"Hermione Granger",
-      "photo":"",
+      "photo":"https://vignette.wikia.nocookie.net/characters/images/a/a5/Latest_%2810%29.jpg/revision/latest?cb=20141230074301",
       "scores":[
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         ""
+         "5",
+         "4",
+         "5",
+         "2",
+         "4",
+         "2",
+         "4",
+         "1",
+         "3",
+         "5"
       ]
    },
    {
       "name":"Ronald Weasley",
-      "photo":"",
+      "photo":"https://www.syfy.com/sites/syfy/files/styles/1200x680/public/wire/legacy/Ron-Weasley.jpg",
       "scores":[
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         ""
+         "2",
+         "4",
+         "2",
+         "3",
+         "3",
+         "2",
+         "4",
+         "1",
+         "3",
+         "5"
       ]
    },
    {
-      "name":"Neville Longbottom",
-      "photo":"",
+      "name":"Draco Malfoy",
+      "photo":"https://cdn.vox-cdn.com/thumbor/3Um79FFZJYXQK6T6-R1s1saX-7A=/0x0:1024x683/1200x800/filters:focal(0x0:1024x683)/cdn.vox-cdn.com/uploads/chorus_image/image/49347607/malfoy.0.0.jpg",
       "scores":[
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         "",
-         ""
+         "4",
+         "3",
+         "4",
+         "5",
+         "1",
+         "5",
+         "1",
+         "4",
+         "1",
+         "3"
       ]
    },
 	{
 	   "name":"Luna Lovegood",
-	   "photo":"",
+	   "photo":"https://images.pottermore.com/bxd3o8b291gf/t6GVMDanqSKGOKaCWi8oi/2a4c50ed04600e4e4c0c5012628f4ace/LunaLovegood_WB_F5_LunaLovegoodPromoCloseUp_Promo_080615_Port.jpg?w=1200",
 	   "scores":[
-	      "",
-	      "",
-	      "",
-	      "",
-	      "",
-	      "",
-	      "",
-	      "",
-	      "",
-	      ""
+	      "3",
+	      "4",
+	      "3",
+	      "1",
+	      "5",
+	      "1",
+	      "5",
+	      "1",
+	      "5",
+	      "4"
 	   ]
 	},
 	{
 	   "name":"Ginny Weasley",
-	   "photo":"",
+	   "photo":"https://4.bp.blogspot.com/-i1Wv240NA-A/WIvVg7oJXLI/AAAAAAAAUvU/UTXyYgjiuAoP6ZVjS-3LRPMTKBwFfbhjACLcB/s1600/ginny-weasley.jpg",
 	   "scores":[
-	      "",
-	      "",
-	      "",
-	      "",
-	      "",
-	      "",
-	      "",
-	      "",
-	      "",
-	      ""
+	      "3",
+	      "4",
+	      "3",
+	      "2",
+	      "4",
+	      "2",
+	      "4",
+	      "1",
+	      "4",
+	      "5"
 	   ]
 	}
 
